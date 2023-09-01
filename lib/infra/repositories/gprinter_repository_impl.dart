4a7cb5b1cac8ea1082216d6fb6c86eea8f4534e4
Joao Mateus <joao.coelho@vrsoft.com.br>
2023-09-01 12:13:08 -0300

    master

--- lib/infra/repositories/gprinter_repository_impl.dart
+++ lib/infra/repositories/gprinter_repository_impl.dart
@@ -2,12 +2,16 @@ import 'package:flutter/services.dart';
 import 'package:vrgpsdk/domain/repositories/gprinter_repository.dart';
 
 class GPrinterRepositoryImpl implements GPrinterRepository {
+  static const String vrGPSdkChannelName = "VR_GPSDK";
+
   final String methodConnectToPrinter = "CONNECT_TO_PRINTER";
   final String methodPrintData = "PRINT_DATA";
 
-  final MethodChannel _methodChannel;
+  late MethodChannel _methodChannel;
 
-  GPrinterRepositoryImpl(this._methodChannel);
+  GPrinterRepositoryImpl() {
+    _methodChannel = const MethodChannel(vrGPSdkChannelName);
+  }
 
   @override
   Future<dynamic> connectToPrinter({
--- lib/shared/vr_gpsdk_consts.dart
+++
@@ -1,3 +0,0 @@
-class VrGPSdkConsts {
-  static const String vrGPSdkChannelName = "VR_GPSDK";
-}
